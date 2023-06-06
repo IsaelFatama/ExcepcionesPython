14dc39072875f0549d86a8e776c22a24cf16fe1d
Isael Javier Fatama Godoy <128384205+IsaelFatama@users.noreply.github.com>
2023-06-06 09:14:04 -0500

    Excepción2.py

--- Excepción2.py
+++ Excepción2.py
@@ -1,14 +1,7 @@
 #Excepción2: ValueError:
-def convert_to_integer(value):
-    try:
-        number = int(value)
-        return number
-    except ValueError as e:
-        print("Error:", str(e))
-        print("Se produjo un ValueError. El valor ingresado no es un número entero válido.")
-        return None
+edad = int(input("Escribe la cantidad de hermanos: "))
 
-user_input = input("Ingrese un número entero: ")
-converted_number = convert_to_integer(user_input)
-if converted_number is not None:
-    print("El número convertido es:", converted_number)
+if edad > 0:
+    print("Tienes más de un hermano")
+else:
+    print("No tienes hermanos")
