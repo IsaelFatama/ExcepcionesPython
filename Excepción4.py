bace3c753bf9093b4819c2f286e510bed1d7adb8
Isael Javier Fatama Godoy <128384205+IsaelFatama@users.noreply.github.com>
2023-06-06 09:05:03 -0500

    Excepción4.py

--- Excepción4.py
+++ Excepción4.py
@@ -1,14 +1,7 @@
-#Excepción 4: KeyError
-def get_value_by_key(dictionary, key):
-    try:
-        return dictionary[key]
-    except KeyError as e:
-        print("Error:", str(e))
-        print("Se produjo un KeyError. La clave no existe en el diccionario.")
-        return None
-
 my_dict = {'a': 1, 'b': 2}
-key = 'c'
-result = get_value_by_key(my_dict, key)
-if result is not None:
-    print("El valor asociado con la clave", key, "es:", result)
+
+try:
+    value = my_dict['c']
+    print(value)
+except KeyError:
+    print("La clave 'c' no existe en el diccionario.")
