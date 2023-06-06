b8473a0ab005afef30680b95aa4b982ad3a68c9d
Isael Javier Fatama Godoy <128384205+IsaelFatama@users.noreply.github.com>
2023-06-06 09:33:37 -0500

    Excepción3.py

--- Excepción3.py
+++ Excepción3.py
@@ -1,14 +1,5 @@
 #Excepción 3: IndexError
-def get_element_by_index(lst, index):
-    try:
-        return lst[index]
-    except IndexError as e:
-        print("Error:", str(e))
-        print("Se produjo un IndexError. El índice está fuera del rango de la lista.")
-        return None
-
-my_list = [1, 2, 3]
-index = 3
-result = get_element_by_index(my_list, index)
-if result is not None:
-    print("El elemento en el índice", index, "es:", result)
+list1 = []
+for i in range (1, 10):
+    list1[i] = i
+print(list1)
